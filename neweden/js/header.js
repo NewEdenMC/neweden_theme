5e0ca4438c6f52c855c874b41829f9264b51b741
aaron <aaron@neweden.co>
2018-07-01 15:56:42 +0100

    Drupal jQuery doesn't use $ as function name

--- neweden/js/header.js
+++ neweden/js/header.js
@@ -1,8 +1,8 @@
-$(document).ready(function() {
+jQuery(document).ready(function() {
 
-    $("header .banner .menus-toggle").click(function() {
-        $(this).children(".menus-icon").toggleClass("open");
-        $(this).siblings(".menus").toggleClass("open");
+    jQuery("header .banner .menus-toggle").click(function() {
+        jQuery(this).children(".menus-icon").toggleClass("open");
+        jQuery(this).siblings(".menus").toggleClass("open");
     });
 
 });
